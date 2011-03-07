dff9226e22d6bd75db103e574b11ee99b3702842
Nicos Karalis <dymytrhy@gmail.com>
2011-03-07 00:28:29 -0300

    Finished layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,12 @@
 module ApplicationHelper
-<<<<<<< HEAD
-=======
 
->>>>>>> filling-in-layout
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
       base_title
     else
-<<<<<<< HEAD
-      "#{@title} | #{base_title}"
-=======
       "#{base_title} | #{@title}"
->>>>>>> filling-in-layout
     end
   end
 end
