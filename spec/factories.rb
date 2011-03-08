afb9714c2aa82abf1d95072fbc9d97959f07452a
Nicos Karalis <dymytrhy@gmail.com>
2011-03-07 21:36:49 -0300

    Minor changes

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,7 @@
-# This will guess the User class
-FactoryGirl.define do
-  # By using the symbol ':user', we get Factory Girl to simulate the User model.
-  factory :user do
-    name                  "Michael Hartl"
-    email                 "mhartl@example.com"
-    password              "foobar"
-    password_confirmation "foobar"
-  end
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
 end
